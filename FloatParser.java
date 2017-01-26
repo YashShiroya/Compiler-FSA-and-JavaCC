15c4d372eac2b40588f892648d268be5e0907d1e
yshiroya <yshiroya@purdue.edu>
2017-01-25 23:51:53 -0500

    Commit

--- FloatParser.java
+++ FloatParser.java
@@ -4,25 +4,26 @@ import java.io.InputStreamReader;
 
 class FloatParser {
   public static void main(String args[]) throws Exception {
-	if (args.length==0) {
-          	System.out.println("Usage: java NumberParser value");
-		System.exit(1);
-        }
-        double val = MyParseFloat(args[0]);
-	System.out.println("Value="+val);
-  }
+     if (args.length==0) {
+       System.out.println("Usage: java NumberParser value");
+       System.exit(1);
+   }
+   double val = MyParseFloat(args[0]);
+   System.out.println("Value="+val);
+}
 
-  enum StateFloat { SSTART, SINTEGER, SDECIMAL, SEND };
+enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT};
 
-  public static double MyParseFloat(String s) throws Exception {
+public static double MyParseFloat(String s) throws Exception {
     // Using the code in DecimalParser.java write a finite state 
     // machine that parses a floating point number of the form 
-    //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)? 
- 
-    return 0;
+    //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
 
 
-  }
+
+  return 0;
+
+}
 
 }
 
