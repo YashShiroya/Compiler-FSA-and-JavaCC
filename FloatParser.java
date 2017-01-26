a2f5c17ae13a1428368b064dd544193ed224ab7f
yshiroya <yshiroya@purdue.edu>
2017-01-26 00:27:27 -0500

    Commit

--- FloatParser.java
+++ FloatParser.java
@@ -11,20 +11,59 @@ class FloatParser {
    double val = MyParseFloat(args[0]);
    System.out.println("Value="+val);
 }
-aa
-enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT};
+
+enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT };
 
 public static double MyParseFloat(String s) throws Exception {
     // Using the code in DecimalParser.java write a finite state 
     // machine that parses a floating point number of the form 
     //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
 
+    StateFloat state;
+    state = StateFloat.S1_INIT;
 
+    int i = 0;
+    double divider = 10;
+    double value = 0;
 
-  return 0;
+    while(i < s.length) {
+
+        char ch = s.charAt(i);
+
+        switch(state) {
+
+            case S1_INIT:
+                
+            break;
+            case S2:
+            break;
+            case S3_ACCEPT:
+            break;
+            case S4:
+            break;
+            case S5:
+            break;
+            case S6_ACCEPT:
+            break;
+            case S7:
+            break;
+            case S8_ACCEPT:
+            break;
+        }
+
+        i++;
+    }
+
+    return 0;
 
 }
 
 }
+
+
+
+
+
+
 
 
