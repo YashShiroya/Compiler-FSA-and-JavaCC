e226b7ef0bd2b35814cf6c0a1cb982f0039b77e0
yshiroya <yshiroya@purdue.edu>
2017-01-26 01:16:51 -0500

    Commit

--- FloatParser.java
+++ FloatParser.java
@@ -3,28 +3,31 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 class FloatParser {
-  public static void main(String args[]) throws Exception {
-     if (args.length==0) {
-       System.out.println("Usage: java NumberParser value");
-       System.exit(1);
-   }
-   double val = MyParseFloat(args[0]);
-   System.out.println("Value="+val);
-}
+    public static void main(String args[]) throws Exception {
+       if (args.length==0) {
+         System.out.println("Usage: java NumberParser value");
+         System.exit(1);
+     }
+     double val = MyParseFloat(args[0]);
+     System.out.println("Value="+val);
+    }
 
-enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT };
+ enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT };
+ enum Sign { POSITIVE, NEGETIVE };
 
-public static double MyParseFloat(String s) throws Exception {
-    // Using the code in DecimalParser.java write a finite state 
-    // machine that parses a floating point number of the form 
-    //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
+ public static double MyParseFloat(String s) throws Exception {
+        // Using the code in DecimalParser.java write a finite state 
+        // machine that parses a floating point number of the form 
+        //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
 
-    StateFloat state;
-    state = StateFloat.S1_INIT;
+    
+    StateFloat state = StateFloat.S1_INIT;
+    Sign sign = Sign.POSITIVE;
 
     int i = 0;
     double divider = 10;
     double value = 0;
+
 
     while(i < s.length()) {
 
@@ -34,13 +37,33 @@ public static double MyParseFloat(String s) throws Exception {
 
             case S1_INIT:
 
-                if(Character.isDigit(ch)) {
-                    state = StateFloat.S3_ACCEPT;
-                    value = Character.getNumericValue(ch);
-                }
-                break;
+            if(Character.isDigit(ch)) {
+                state = StateFloat.S3_ACCEPT;
+                value = Character.getNumericValue(ch);
+                i++;
+            }
+
+            else if(ch == '+') { 
+                sign = Sign.POSITIVE;
+                state = StateFloat.S2;
+            }
+
+            else if(ch == '-') {
+                sign = Sign.NEGETIVE;
+                state = StateFloat.S2;
+            }
+
+            else if(ch == '.') {
+
+            }
+
+            else throw new Exception("Bad format");
+            break;
 
             case S2:
+
+             
+
             break;
             case S3_ACCEPT:
             break;
@@ -55,8 +78,6 @@ public static double MyParseFloat(String s) throws Exception {
             case S8_ACCEPT:
             break;
         }
-
-        i++;
     }
 
     return 0;
