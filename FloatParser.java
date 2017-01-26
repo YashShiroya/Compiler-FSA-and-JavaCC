b752304d1c3b07f6b283bba1428c86248187d6e9
yshiroya <yshiroya@purdue.edu>
2017-01-26 01:44:23 -0500

    Commit

--- FloatParser.java
+++ FloatParser.java
@@ -4,18 +4,18 @@ import java.io.InputStreamReader;
 
 class FloatParser {
     public static void main(String args[]) throws Exception {
-     if (args.length==0) {
-       System.out.println("Usage: java NumberParser value");
-       System.exit(1);
-   }
-   double val = MyParseFloat(args[0]);
-   System.out.println("Value="+val);
-}
+       if (args.length==0) {
+         System.out.println("Usage: java NumberParser value");
+         System.exit(1);
+    }
+    double val = MyParseFloat(args[0]);
+    System.out.println("Value="+val);
+    }
 
-enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT };
-enum Sign { POSITIVE, NEGETIVE };
+ enum StateFloat { S1_INIT, S2, S3_ACCEPT, S4, S5, S6_ACCEPT, S7, S8_ACCEPT };
+ enum Sign { POSITIVE, NEGETIVE };
 
-public static double MyParseFloat(String s) throws Exception {
+ public static double MyParseFloat(String s) throws Exception {
         // Using the code in DecimalParser.java write a finite state 
         // machine that parses a floating point number of the form 
         //             [-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
@@ -33,7 +33,49 @@ public static double MyParseFloat(String s) throws Exception {
 
         char ch = s.charAt(i);
 
+        switch(state) {
+
+            case S1_INIT:
+
+            if(Character.isDigit(ch)) {
+                state = StateFloat.S3_ACCEPT;
+                value = Character.getNumericValue(ch);
+                i++;
+            }
+
+            else if(ch == '+') { 
+                sign = Sign.POSITIVE;
+                state = StateFloat.S2;
+            }
+
+            else if(ch == '-') {
+                sign = Sign.NEGETIVE;
+                state = StateFloat.S2;
+            }
+
+            else if(ch == '.') {
+                state = StateFloat.S4;
+            }
+
+            else throw new Exception("Bad format");
+            break;
+
+            case S2:
+
+            if(Character.isDigit(ch)) {
+                state = StateFloat.S3_ACCEPT;
+                value = Character.getNumericValue(ch);
+            }
+            else if(ch == '.') {
+                state = StateFloat.S4;
+            }
+
+            break;
+
         
+
+            }
+        }
     }
 
     return 0;
